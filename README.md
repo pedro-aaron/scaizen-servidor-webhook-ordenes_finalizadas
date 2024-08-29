592c9cba5dda8653115f7ea32e6fb1e7566b2fad
Pedro Aarón <pedro.aaron@gmail.com>
2024-08-29 15:35:04 -0600

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,180 @@
-# scaizen-servidor-webhook-ordenes_finalizadas
- Servidor Webhook para recibir notificaciones de ordenes finalizadas de SCAIZEN
+<!-- seccion -->
+
+# Ejemplo de Servidor Webhook para recibir notificaciones de ordenes finalizadas de SCAIZEN
+
+## instalar dependencias
+
+```bash
+pip3 install -r requirements.txt
+```
+
+## correr el server
+
+```bash
+python3 server_webhook.py
+```
+
+## Documentación server
+
+-   http://localhost:8787/docs
+
+## Credenciales de acceso
+
+-   Username: johndoe
+-   Password: secret
+
+# Requerimientos
+
+El servidor webhook debe ser capaz de recibir notificaciones de ordenes finalizadas de SCAIZEN.
+
+## 1. Autenticación
+
+El endpoint para obtener el token de acceso tiene el formato siguiente:
+
+```http
+<HOST_SERVIDOR>/token
+```
+
+### 1.1 Endpoint para obtener el token de acceso
+
+Suponiendo que el HOST_SERVIDOR es `http://localhost:8787`, el endpoint sería `http://localhost:8787/token`.
+
+### 1.2 Petición para obtener el token de acceso
+
+La petición para obtener el token de acceso debe ser de tipo `POST`. A continuación se muestra un ejemplo de la petición usando CURL:
+
+```bash
+curl -X 'POST' \
+  'http://127.0.0.1:8787/token' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -d 'username=johndoe&password=secret'
+```
+
+SCAIZEN enviará en la petición los siguientes headers (como se aprecia en el ejemplo de CURL):
+
+-   Header `Content-Type` con el valor `application/x-www-form-urlencoded`.
+-   Header `accept` con el valor `application/json`.
+
+### 1.3 Respuesta de la petición
+
+La respuesta de la petición será un objeto JSON con el token de acceso. Las llaves del objeto son `access_token` y `token_type`. El valor de `access_token` es el token de acceso que se debe enviar en el header `Authorization` de las peticiones al servidor webhook. El valor de `token_type` es el tipo de token, en este caso se espera `bearer`.
+
+A continuación se muestra un ejemplo de la respuesta:
+
+```json
+{
+    "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJqb2huZG9lIiwiZXhwIjoxNzI0OTYzNDI4fQ.cy_G-uOQHMgAUK_yW8vqXSnlRu4Fo7R4NPjzwIT3shU",
+    "token_type": "bearer"
+}
+```
+
+## 2. Webhook de orden finalizada
+
+La URL para recibir la notificación de orden finalizada tiene un formato similar al siguiente:
+
+```http
+<HOST_SERVIDOR>/<ENDPOINT>
+```
+
+### 2.1 Endpoint para recibir la notificación de orden finalizada
+
+Suponiendo que el HOST_SERVIDOR es `http://localhost:8787` y ENDPOINT es `webhook_scaizen_finalizacion_orden`, la URL sería `http://localhost:8787/webhook_scaizen_finalizacion_orden`.
+
+### 2.2 Recepción del evento de orden finalizada
+
+Una vez que SCAIZEN envíe la notificación de orden finalizada, el servidor webhook debe responder con un código HTTP `200` si la notificación fue recibida exitosamente, de lo contrario, debe responder con un código HTTP `400` u otro correspondiente al error.
+
+SCAIZEN enviará al webhook (mediante POST) la siguiente información de la orden finalizada:
+
+```json
+{
+    "timestamp": "2024-08-29 20:34:57",
+    "id_orden": 124,
+    "tipo": "carga",
+    "producto": "diesel",
+    "volumen_natural": 123.45,
+    "volumen_neto": 123.456,
+    "densidad": 1.254,
+    "temperatura": 28.125,
+    "fecha_inicio": "2024-08-29 20:34:57",
+    "fecha_fin": "2024-08-29 20:34:57"
+}
+```
+
+donde:
+
+-   `timestamp` es la fecha y hora en la que se envió la notificación. Formato: YYYY-MM-DD HH:MM:SS
+-   `id_orden` es el identificador único de la orden.
+-   `tipo` es el tipo de orden. Posibles valores: carga, descarga, reingreso.
+-   producto es el producto involucrado en la orden. Posibles valores: diesel, magna, premium.
+-   `volumen_natural` es el volumen natural de la operación.
+-   `volumen_neto` es el volumen neto de la operación.
+-   `densidad` es la densidad del producto.
+-   `temperatura` es la temperatura del producto.
+-   `fecha_inicio` es la fecha y hora de inicio de la operación. Formato: YYYY-MM-DD HH:MM:SS
+-   `fecha_fin` es la fecha y hora de fin de la operación. Formato: YYYY-MM-DD HH:MM:SS
+
+A continuación se muestra un ejemplo de la petición que ejecutará SCAIZEN al servidor webhook usando CURL:
+
+```bash
+curl -X 'POST' \
+  'http://127.0.0.1:8787/webhook_scaizen_finalizacion_orden' \
+  -H 'accept: application/json' \
+  -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJqb2huZG9lIiwiZXhwIjoxNzI0OTY2NTE3fQ.pH8ExYMUijmiXxVhOIxgbRpAipk7xqJHP1gQP2FwWS0' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "timestamp": "2024-08-29 20:34:57",
+  "id_orden": 124,
+  "tipo": "carga",
+  "producto": "diesel",
+  "volumen_natural": 123.45,
+  "volumen_neto": 123.456,
+  "densidad": 1.254,
+  "temperatura": 28.125,
+  "fecha_inicio": "2024-08-29 20:34:57",
+  "fecha_fin": "2024-08-29 20:34:57"
+}'
+
+```
+
+Es importante que se desarrolle en webhook siguiendo la estructura mostrada en el ejemplo de CURL, de lo contrario no se podrá recibir la notificación de orden finalizada.
+
+SCAIZEN enviará en la petición los siguientes headers (como se aprecia en el ejemplo de CURL):
+
+-   Header `Authorization` con el token de acceso obtenido en el paso anterior.
+-   Header `Content-Type` con el valor `application/json`.
+-   Header `accept` con el valor `application/json`.
+
+### 2.3 Respuesta de la petición
+
+La respuesta de la petición puede ser un objeto JSON con un mensaje de confirmación. A continuación se muestra un ejemplo de la respuesta:
+
+```json
+{
+    "status": "success",
+    "message": "Orden procesada con éxito."
+}
+```
+
+No obstante, la respuesta puede variar dependiendo de la implementación del servidor webhook.
+
+SCAIZEN sólo espera un código de respuesta `200` para confirmar que la notificación fue recibida exitosamente.
+
+Si el código de respuesta es diferente a `200`, SCAIZEN considerará que la notificación no fue recibida y volverá a intentar enviarla.
+
+# RESUMEN DE REQUERIMIENTOS
+
+Resumen de los requerimientos para el diseño del servidor webhook:
+
+1. Valor de `<HOST_SERVIDOR>`
+2. Valor de `<USERNAME>`
+3. Valor de `<ENDPOINT>`
+4. Valor de `<PASSWORD>`
+5. La url para autenticación debe ser `<HOST_SERVIDOR>/token`
+6. Los headers de la petición de autenticación deben ser `Content-Type: application/x-www-form-urlencoded` y `accept: application/json`
+7. el body de la petición de autenticación debe ser `username=<USERNAME>&password=<PASSWORD>`
+8. La url para enviar la notificación de orden finalizada debe ser `<HOST_SERVIDOR>/<ENDPOINT>`
+9. El body de la petición de la notificación de orden finalizada debe ser un objeto JSON con los campos mencionaos en el punto 2.2: `timestamp`, `id_orden`, `tipo`, `producto`, `volumen_natural`, `volumen_neto`, `densidad`, `temperatura`, `fecha_inicio`, `fecha_fin`
+10. Los headers de la petición de la notificación de orden finalizada deben ser como los mencionados en el punto 2.2: `Authorization`, `Content-Type`, `accept`
+11. La respuesta de la petición de la notificación de orden finalizada debe ser un código de respuesta `200` si la notificación fue recibida exitosamente, de lo contrario, debe responder con un código HTTP `400` u otro correspondiente al error.
---
+++ server_webhook.py
@@ -0,0 +1,259 @@
+# Código tomado de https://fastapi.tiangolo.com/tutorial/security/oauth2-jwt/
+
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
+
+import jwt
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jwt.exceptions import InvalidTokenError
+from passlib.context import CryptContext
+from pydantic import BaseModel, Field
+
+import uvicorn
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    }
+}
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+app = FastAPI()
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
+
+@app.get("/users/me/", response_model=User)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return current_user
+
+
+@app.get("/users/me/items/")
+async def read_own_items(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return [{"item_id": "Foo", "owner": current_user.username}]
+
+
+# -------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
+# Código que gestional el webhook de SCAIZEN
+# -------------------------------------------------------------------------------
+# Esquema para el mensaje que recibirá el endpoint
+class OrdenFinalizada(BaseModel):
+    timestamp: datetime = Field(
+        ...,
+        title="Fecha y hora del mensaje. Formato: YYYY-MM-DD HH:MM:SS",
+        description="Fecha y hora del mensaje. Formato: YYYY-MM-DD HH:MM:SS",
+    )
+    id_orden: int = Field(
+        ...,
+        title="ID de la orden SCAIZEN",
+        description="ID de la orden SCAIZEN",
+    )
+    # los posibles valores de tipo son: "carga", "descarga" o "reingreso"
+    tipo: str = Field(
+        ...,
+        title="Tipo de operación",
+        description="Tipo de operación, puede ser: 'carga', 'descarga' o 'reingreso'",
+    )
+    producto: str = Field(
+        ...,
+        title="Producto",
+        description="Producto, puede ser: 'magna', 'diesel' o 'premium'",
+    )
+    volumen_natural: float = Field(
+        ...,
+        title="Volumen natural",
+        description="Volumen natural del producto",
+    )
+    volumen_neto: float = Field(
+        ...,
+        title="Volumen neto",
+        description="Volumen neto del producto",
+    )
+    densidad: float = Field(
+        ...,
+        title="Densidad",
+        description="Densidad del producto",
+    )
+    temperatura: float = Field(
+        ...,
+        title="Temperatura",
+        description="Temperatura del producto",
+    )
+    fecha_inicio: datetime = Field(
+        ...,
+        title="Fecha de inicio",
+        description="Fecha de inicio de la operación. Formato: YYYY-MM-DD HH:MM:SS",
+    )
+    fecha_fin: datetime = Field(
+        ...,
+        title="Fecha de fin",
+        description="Fecha de fin de la operación. Formato: YYYY-MM-DD HH:MM:SS",
+    )
+
+
+# -------------------------------------------------------------------------------
+# Endpoint para recibir el mensaje de SCAIZEN
+# Funcionamiento:
+# - Recibe un mensaje en formato JSON
+# - Verifica si el id_orden es par o impar
+# - Si es par, devuelve un mensaje de éxito
+# - Si es impar, devuelve un mensaje de error
+# -------------------------------------------------------------------------------
+@app.post("/webhook_scaizen_finalizacion_orden")
+async def scaizen_finalizacion_orden_webhook(
+    data: OrdenFinalizada,
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    print(f"---------------------------------------------------------")
+    print(f"---------------------------------------------------------")
+    print(f"Mensaje recibido:")
+    print(f"---------------------------------------------------------")
+    print(f"{data}")
+    print(f"---------------------------------------------------------")
+    print(f"---------------------------------------------------------")
+
+    # Verifica si el id_orden es par o impar
+    if data.id_orden % 2 == 0:
+        retorn = {"status": "success", "message": "Orden procesada con éxito."}
+        print(f"-----RESPUESTA OK-----")
+        print(f"Orden {data.id_orden} procesada con éxito.")
+        print(f"---------------------------------------------------------")
+        return retorn
+    else:
+        print(f"-----RESPUESTA ERROR-----")
+        print(f"Orden {data.id_orden} no se puede procesar.")
+        print(f"---------------------------------------------------------")
+        raise HTTPException(
+            status_code=400, detail="ID de orden impar. No se puede procesar."
+        )
+
+
+# home implementation
+@app.get("/")
+async def home():
+    return {
+        "message": "Bienvenido, la documentación de la API se encuentra en /docs: http://127.0.0.1:8787/docs"
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8787)
